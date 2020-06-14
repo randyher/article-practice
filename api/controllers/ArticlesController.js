ad073630ae7ada23a11cf50c508058cae750879c
github username <randyherasme6@gmail.com>
2020-06-14 14:14:13 -0400

    list page

--- api/controllers/ArticlesController.js
+++ api/controllers/ArticlesController.js
@@ -6,7 +6,13 @@
  */
 
 module.exports = {
-  
-
+  list: function(req, res) {
+    Articles.find({}).exec((err, articles) => {
+      console.log("Here:", articles);
+      if (err) {
+        res.send(500, { error: "Database Error" });
+      }
+      res.view("list", { articles: articles });
+    });
+  }
 };
-
